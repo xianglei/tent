2f78e74ec8e307bfd11fa80f2237ba000b0d38ab
xianglei <xianglei@exadoop.com>
2018-02-28 01:39:00 +0800

    torch version

--- tent-packages/src/common/pylearning3/centos/6/pip.sh
+++ tent-packages/src/common/pylearning3/centos/6/pip.sh
@@ -8,5 +8,5 @@ sudo ./pip3 install aerospike amqp alluxio apscheduler automl avro awscli beauti
  requests django==1.11 flask scipy seaborn simplejson sklearn sklearn-crfsuite sklearn-pandas snownlp spark-sklearn statsmodels tabulate teradata theano \
  thrift thriftpy tornado tqdm xgboost==0.4a30 opencv-contrib-python pyspark-hbase XlsxWriter PyStemmer python-lzo lzo-indexer word2vec scrapy virtualenv
 
-sudo ./pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp27-cp27m-linux_x86_64.whl
+sudo ./pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 sudo ./pip3 install torchvision
