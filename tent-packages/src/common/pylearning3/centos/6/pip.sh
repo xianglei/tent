ccb768b92da6388f21e3d2591aba61095da6fbe5
xianglei <xianglei@exadoop.com>
2018-02-28 01:31:49 +0800

    pip3

--- tent-packages/src/common/pylearning3/centos/6/pip.sh
+++ tent-packages/src/common/pylearning3/centos/6/pip.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo ./pip install requests six numpy h5py cython
-sudo ./pip install aerospike amqp alluxio apscheduler automl avro awscli beautifulsoup4 bigml cassandra-driver celery opencv-python cx_oracle \
+sudo ./pip3 install requests six numpy h5py cython
+sudo ./pip3 install aerospike amqp alluxio apscheduler automl avro awscli beautifulsoup4 bigml cassandra-driver celery opencv-python cx_oracle \
  easybase elasticsearch fabric freeze graphviz gensim Geohash gevent greenlet python-gssapi gym hbase-thrift hive-thrift-py jieba kafka kazoo keras kerberos \
  kombu log4python matplotlib mlpy mrjob networkx nltk pandas pandas2sklearn pandas-oracle paramiko parquet pattern pika protobuf py4j \
  pyaccumulo pyaml python-crfsuite pyinstaller pymongo pymssql pymysql pyspark python-cassandra python-crfsuite python-memcached python-snappy redis \
  requests django==1.11 flask scipy seaborn simplejson sklearn sklearn-crfsuite sklearn-pandas snownlp spark-sklearn statsmodels tabulate teradata theano \
  thrift thriftpy tornado tqdm xgboost==0.4a30 opencv-contrib-python pyspark-hbase XlsxWriter PyStemmer python-lzo lzo-indexer word2vec scrapy virtualenv
 
-sudo ./pip install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp27-cp27m-linux_x86_64.whl
-sudo ./pip install torchvision
+sudo ./pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp27-cp27m-linux_x86_64.whl
+sudo ./pip3 install torchvision
