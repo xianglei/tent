4dda8a0620688a5a0adf0172a1b592de4e9b2cde
xianglei <xianglei@admaster.com.cn>
2018-02-27 13:27:43 +0800

    init with pylearning2

--- tent-packages/src/common/env.sh
+++ tent-packages/src/common/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-OS_VERSION=$(lsb_release -a | grep Release | awk '{print $2}')
+OS_VERSION=$(lsb_release -a | grep Release | awk '{print $2}' | awk -F'.' '{print $1}')
 OS_RELEASE=$(lsb_release -a | grep Distributor | awk '{print $3}' | tr 'A-Z' 'a-z')
 OS_CODENAME=$(lsb_release -a | grep Codename | awk '{print $2}')
