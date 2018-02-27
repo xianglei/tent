1830db6793cec63ab9c8e191178af3029a64210b
xianglei <xianglei@admaster.com.cn>
2018-02-27 13:21:57 +0800

    init with pylearning2

--- tent-packages/src/common/env.sh
+++ tent-packages/src/common/env.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-BASEDIR=$(cd `dirname $0`; pwd)
-DOWNLOADROOT=${BASEDIR}/../../../../dl
-OUTPUTROOT=${BASEDIR}/../../../../output
-BUILDROOT=${BASEDIR}/../../../../build
-
 OS_VERSION=$(lsb_release -a | grep Release | awk '{print $2}')
 OS_RELEASE=$(lsb_release -a | grep Distributor | awk '{print $3}' | tr 'A-Z' 'a-z')
 OS_CODENAME=$(lsb_release -a | grep Codename | awk '{print $2}')
