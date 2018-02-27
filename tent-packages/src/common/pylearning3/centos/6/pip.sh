c8ba350d69e310d812f23e2d13a6270f0907439a
xianglei <xianglei@exadoop.com>
2018-02-28 01:44:47 +0800

    torch version

--- tent-packages/src/common/pylearning3/centos/6/pip.sh
+++ tent-packages/src/common/pylearning3/centos/6/pip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo ./pip3 install requests six numpy h5py cython
+sudo ./pip3 install requests six numpy h5py cython option
 sudo ./pip3 install aerospike amqp alluxio apscheduler automl avro awscli beautifulsoup4 bigml cassandra-driver celery opencv-python cx_oracle \
  easybase elasticsearch fabric freeze graphviz gensim Geohash gevent greenlet python-gssapi gym hbase-thrift hive-thrift-py jieba kafka kazoo keras kerberos \
  kombu log4python matplotlib mlpy mrjob networkx nltk pandas pandas2sklearn pandas-oracle paramiko parquet pattern pika protobuf py4j \
