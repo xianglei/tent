aa26f6dd28cdf48083d745982114df335ccf3d8f
xianglei <xianglei@admaster.com.cn>
2018-02-27 17:09:31 +0800

    init with pylearning2

--- Modules/building.py
+++ Modules/building.py
@@ -36,21 +36,21 @@ class Builder(OSDist):
         dest = self.root + 'tent-packages/src/common/env.sh'
         dest_str = \
         '''
-        #!/bin/sh
-        OS_VERSION=$(lsb_release -a | grep Release | awk '{print $2}' | awk -F'.' '{print $1}')
-        OS_RELEASE=$(lsb_release -a | grep Distributor | awk '{print $3}' | tr 'A-Z' 'a-z')
-        OS_CODENAME=$(lsb_release -a | grep Codename | awk '{print $2}')
-        if [[ $OS_RELEASE == 'centos' || $OS_RELEASE == 'redhat' ]]; then
-            if [ ! -e /etc/yum.repos.d/epel.repo ]; then
-                if [ $OS_VERSION == '6' ]; then
-                    wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
-                fi
-                if [ $OS_VERSION == '7' ]; then
-                    wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-                fi
-                sudo yum makecache
-            fi
+#!/bin/sh
+OS_VERSION=$(lsb_release -a | grep Release | awk '{print $2}' | awk -F'.' '{print $1}')
+OS_RELEASE=$(lsb_release -a | grep Distributor | awk '{print $3}' | tr 'A-Z' 'a-z')
+OS_CODENAME=$(lsb_release -a | grep Codename | awk '{print $2}')
+if [[ $OS_RELEASE == 'centos' || $OS_RELEASE == 'redhat' ]]; then
+    if [ ! -e /etc/yum.repos.d/epel.repo ]; then
+        if [ $OS_VERSION == '6' ]; then
+            wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
         fi
+        if [ $OS_VERSION == '7' ]; then
+            wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+        fi
+        sudo yum makecache
+    fi
+fi
         
         '''
         dest_str += 'BASE_VERSION=' + package_def['base-version'] + '\n'
