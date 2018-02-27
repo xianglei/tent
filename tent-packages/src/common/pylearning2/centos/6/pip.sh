e03c95c90f6fc6f0e0ef20f32274682e9f10fae9
xianglei <xianglei@admaster.com.cn>
2018-02-27 13:34:49 +0800

    init with pylearning2

--- tent-packages/src/common/pylearning2/centos/6/pip.sh
+++ tent-packages/src/common/pylearning2/centos/6/pip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+sudo ./pip install requests
 sudo ./pip install aerospike amqp alluxio apscheduler avro awscli beautifulsoup4 bigml cassandra-driver celery opencv-python cx_oracle cython \
  easybase elasticsearch fabric freeze gensim Geohash gevent greenlet python-gssapi gym hbase-thrift hive-thrift-py jieba kafka kazoo keras kerberos \
  kombu log4python matplotlib mlpy mrjob networkx nltk numpy pandas pandas2sklearn pandas-oracle paramiko parquet pattern pika protobuf py4j \
@@ -8,5 +8,5 @@ sudo ./pip install aerospike amqp alluxio apscheduler avro awscli beautifulsoup4
  requests django==1.11 flask scipy seaborn simplejson sklearn sklearn-crfsuite sklearn-pandas snownlp spark-sklearn statsmodels tabulate teradata theano \
  thrift thriftpy tornado tqdm xgboost==0.4a30
 
-sudo ./pip install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
+sudo ./pip install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp27-cp27m-linux_x86_64.whl
 sudo ./pip install  torchvision
