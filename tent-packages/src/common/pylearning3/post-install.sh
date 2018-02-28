4b784a8c051670362c8d35ea33cca09cb7adad85
xianglei <xianglei@admaster.com.cn>
2018-02-28 13:44:04 +0800

    optimize 3

--- tent-packages/src/common/pylearning3/post-install.sh
+++ tent-packages/src/common/pylearning3/post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-/bin/ln -sf /usr/lib/pylearning3/lib/libpython3.6m.a /usr/lib64
+/bin/ln -sf /usr/lib/pylearning3/lib/libpython3.6m.so /usr/lib64
+/bin/ln -sf /usr/lib/pylearning3/lib/libpython3.6m.so.1.0 /usr/lib64
 /bin/ln -sf /usr/lib/pylearning3/bin/python /usr/bin/pylearning3
 /bin/chmod 755 /usr/bin/pylearning3
--- tent-packages/src/common/pylearning3/post-uninstall.sh
+++ tent-packages/src/common/pylearning3/post-uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 /bin/rm -rf /usr/lib/pylearning3
-/bin/rm -f /usr/lib64/libpython3.6m.a
+/bin/rm -f /usr/lib64/libpython3.6m.so
+/bin/rm -f /usr/lib64/libpython3.6m.so.1.0
 /bin/rm -f /usr/bin/pylearning3
