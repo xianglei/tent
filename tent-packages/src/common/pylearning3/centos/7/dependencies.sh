ac31821628911b1b31953053d6dc769002c66cd8
xianglei <xianglei@admaster.com.cn>
2018-06-01 14:58:45 +0800

    optimized: auto solve pip and python dependencies

---
+++ tent-packages/src/common/pylearning3/centos/7/dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sudo yum -y install \
+    lapack lapack-devel \
+    blas blas-devel \
+    freetds freetds-devel \
+    opencv opencv-devel \
+    tk tk-devel \
+    cyrus-sasl cyrus-sasl-devel \
+    openssl openssl-devel \
+    snappy snappy-devel \
+    libSM libSM-devel \
+    libffi-devel \
+    sqlite-devel \
+    ncurses-devel \
+    libstdc++ libstdc++-devel \
+    gcc gcc-c++ \
+    glibc glibc-devel \
+    krb5-devel \
+    mariadb-devel \
+    make cmake automake autoconf libtool pkgconf m4
